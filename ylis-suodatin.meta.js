1419d4506ac3a8a1a0b1066e482f59e7c84cb8e2
testuser3158 <testuser3158@protonmail.com>
2021-10-06 13:08:22 +0300

    v1.0.0

---
+++ ylis-suodatin.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        ylis-suodatin
+// @version     1.0.0
+// @include     /^https:\/\/ylilauta.org\/[^/]+/.*/
+// @namespace   github:testuser3158/ylis-suodatin
+// @updateURL   https://testuser3158.github.io/ylis-suodatin/ylis-suodatin.meta.js
+// @downloadURL https://testuser3158.github.io/ylis-suodatin/ylis-suodatin.user.js
+// ==/UserScript==
